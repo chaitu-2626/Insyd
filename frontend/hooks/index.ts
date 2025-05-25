551301e27cf07be69053060dc3a52bbce94f7ebf
chaitu-2626 <m.vinaychaitanya@gmail.com>
2025-05-25 21:29:07 +0530

    Removed unused files

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -1,4 +1,3 @@
 export { default as useCreatePost } from './useCreatePost';
 export { default as useUserSession } from './useUserSession';
 export {default as usePosts} from './usePosts';
-export {default as useSocket} from './useSocket'
